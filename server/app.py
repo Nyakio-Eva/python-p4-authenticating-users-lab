2b3ef6c4c276aaf1a189f7f87ef63dd12b08939a
Nyakio-Eva <evalkariuki@gmail.com>
2024-04-14 22:38:54 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -48,10 +48,47 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+
+    def post(self):
+        data = request.get_json()
+
+        username = data['username']
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            return make_response(jsonify({'error': 'User not found'}), 404)
+
+        session['user_id'] = user.id
+        return make_response(jsonify(user.to_dict()), 200)
+
+class Logout(Resource):
+
+    def delete(self):
+        session['user_id'] = None
+        return{
+            "message": "204: No data "
+        },204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        user = User.query.filter_by(id=user_id).first()
+
+        if user:
+            response = jsonify(user.to_dict())
+            response.status_code = 200
+            return response
+        else:
+            return {}, 401
+        
+
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
